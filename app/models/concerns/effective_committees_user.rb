62b088fa416cea3c0e92f3a24fb0a9521ee8340b
Matt Riemer <matthew@codeandeffect.com>
2023-11-10 12:15:42 -0700

    Add with_committee scope to committee user

--- app/models/concerns/effective_committees_user.rb
+++ app/models/concerns/effective_committees_user.rb
@@ -20,6 +20,14 @@ module EffectiveCommitteesUser
       class_name: 'Effective::CommitteeMember', inverse_of: :user, dependent: :delete_all
 
     accepts_nested_attributes_for :committee_members, allow_destroy: true
+
+    scope :with_committee, -> (committees) {
+      committees = Resource.new(Effective::Committee).search_any(committees) if committees.kind_of?(String)
+      raise('expected an Effective::Committee') unless committees.present? && Array(committees).all? { |c| c.kind_of?(Effective::Committee) }
+
+      committee_members = Effective::CommitteeMember.where(committee: committees).where(user_type: name)
+      where(id: committee_members.select(:user_id))
+    }
   end
 
   # Instance Methods
