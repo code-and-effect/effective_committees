e8d3f2fb3c5ef0737145baf46f6c27503a6a67af
Matt Riemer <matthew@codeandeffect.com>
2026-02-13 09:41:16 -0700

    Bulk moving files

--- app/controllers/admin/committee_files_controller.rb
+++ app/controllers/admin/committee_files_controller.rb
@@ -6,14 +6,23 @@ module Admin
     include Effective::CrudController
 
     def bulk_move
-      @committee_files = Effective::CommitteeFile.where(id: params[:ids])
-      @committee_folder = Effective::CommitteeFolder.find_by_id(params[:committee_folder_id])
+      committee_folder = Effective::CommitteeFolder.find_by_id(params[:committee_folder_id])
+      committee_files = Effective::CommitteeFile.sorted.where(id: params[:ids])
+      original_folder = Effective::CommitteeFolder.find_by_id(committee_files.first.committee_folder_id)
 
-      @committee_files.each do |committee_file|
-        committee_file.update!(committee_folder: @committee_folder, position: nil)
+      # Update the position of the files in the new folder
+      position = (committee_folder.committee_files.map { |file| file.position }.compact.max || -1) + 1
+
+      committee_files.each_with_index do |committee_file, index|
+        committee_file.update!(committee_folder: committee_folder, position: position + index)
       end
 
-      render json: { status: 200, message: "Successfully moved #{@committee_files.length} files to #{@committee_folder.title}" }
+      # Update the position of the files in the original folder
+      original_folder.committee_files.each_with_index do |committee_file, index|
+        committee_file.update!(position: index)
+      end
+
+      render json: { status: 200, message: "Successfully moved #{committee_files.length} files to #{committee_folder.title}" }
     rescue => e
       render json: { status: 500, message: "Unable to move files: #{e.message}" }
     end
